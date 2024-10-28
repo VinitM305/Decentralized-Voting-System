26de0c6271d3bf0cd7642f1f640673c31e96302b
VinitM305 <114692166+VinitM305@users.noreply.github.com>
2024-10-29 02:05:30 +0530

    voter view updated

--- components/voterCard/voterCard.jsx
+++ components/voterCard/voterCard.jsx
@@ -5,8 +5,28 @@ import Style from '../card/card.module.css';
 import images from '../../assets';
 import voterCardStyle from './voterCard.module.css';
 
-const voterCard = () => {
-  return <div>voterCard</div>;
+const voterCard = ({voterArray, voterLength}) => {
+  return (
+    <div className={Style.card}>
+      {voterArray.map((el,i) => (
+        <div className={Style.card_box}>
+          <div className={Style.image}>
+            <img src={el[4]} alt="Profile photo"/>
+          </div>
+          <div className={Style.card_info}>
+            <h2>
+              {el[1]} #{el[0].toNumber()}
+            </h2>
+            <p>Address: {el[3].slice(0,30)}...</p>
+            <p>Details</p>
+            <p className={voterCardStyle.vote_Status}>
+              {el[6] == true ? "You Already Voted" : "Not Voted"}
+            </p> 
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 };
 
 export default voterCard;
--- pages/voterList.js
+++ pages/voterList.js
@@ -1,16 +1,31 @@
 import React, {useState, useEffect, useContext} from "react";
 
-import voterCard from '../components/voterCard/voterCard';
+import VoterCard from '../components/voterCard/voterCard';
 import Style from '../styles/voterList.module.css';
 import {VotingContext} from '../context/Voter';
+import useVoters from "./useVoters";
 
 const voterList = () => {
-
+  const {checkIfWalletIsConnected} = useContext(VotingContext);
+  const { voterArray, voterLength } = useVoters();
+  const [showCard, setShowCard] = useState(false);
   useEffect(() => {
+    checkIfWalletIsConnected();
+    
+    const timer = setTimeout(() => {
+      setShowCard(true); // Set to true after 2 seconds
+    }, 2000);
 
-  },[]);
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, [checkIfWalletIsConnected]);
 
-  return <div>voterList</div>;
+  return (
+    <div className={Style.voterList}>
+      {showCard && ( 
+        <VoterCard voterArray={voterArray} />
+      )}
+    </div>
+  )
 };
 
 export default voterList;
